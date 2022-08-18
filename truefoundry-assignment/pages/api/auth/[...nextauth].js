6ed8e80a0f9b964322275e58d3867270e9ecefdc
Aditi Tiwari <adititiwari@192.168.1.13>
2022-08-18 13:08:19 +0530

    Added the functionality for creating repo

--- truefoundry-assignment/pages/api/auth/[...nextauth].js
+++ truefoundry-assignment/pages/api/auth/[...nextauth].js
@@ -1,11 +1,30 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+// import clientPromise from "../../../database/connectDB";
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        url: "https://github.com/login/oauth/authorize",
+        params: { scope: "read:user user:email repo" },
+      },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  // adapter: MongoDBAdapter(clientPromise),
 });
--- truefoundry-assignment/pages/index.js
+++ truefoundry-assignment/pages/index.js
@@ -1,31 +1,132 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 
-import Card from "react-bootstrap/Card";
-
 export default function Home() {
   const { data: session } = useSession();
+  const [ifAddRepo, setIfAddRepo] = useState(false);
+  const [repoData, setRepoData] = useState();
+
+  function getUserIdFromImageURL(imageUrl) {
+    var id = "";
+    var numberOfSlash = 0;
+    for (var ch in imageUrl) {
+      if (imageUrl.charAt(ch) === "?") break;
+      else if (numberOfSlash == 4) id += imageUrl.charAt(ch);
+      else if (imageUrl.charAt(ch) === "/") numberOfSlash += 1;
+    }
+    return id;
+  }
+
+  async function repoDataURL() {
+    var userId = getUserIdFromImageURL(session.user.image);
+    await fetch(`https://api.github.com/user/${userId}/repos`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          const list = result.map((item) => (
+            <div className="text-center">
+              {console.log(item.name)}
+              <a target="_blank" href={item.svn_url}>
+                {item.name}
+              </a>
+            </div>
+          ));
+          setRepoData(list);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  function useInput({ type, placeholder }) {
+    const [value, setValue] = useState("");
+    const input = (
+      <input
+        className={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type={type}
+      />
+    );
+    return [value, input];
+  }
+
+  const [repoName, repoNameInput] = useInput({
+    type: "text",
+    placeholder: "Enter Repo Name",
+  });
+  const [repoDescription, repoDescriptionInput] = useInput({
+    type: "text",
+    placeholder: "Enter Repo Description",
+  });
+
+  async function addRepo() {
+    const url = "https://api.github.com/user/repos";
+    const token = session.accessToken;
+    const headers = {
+      Authorization: `Token ${token}`,
+    };
+    const payLoad = {
+      name: repoName,
+      description: repoDescription,
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(payLoad),
+    });
+    const result = await response.json();
+    console.log(result);
+    setIfAddRepo(!ifAddRepo);
+  }
 
   return (
     <div className={styles.container}>
       {!session && (
-        <button onClick={() => signIn()}>Sign in with Github</button>
+        <button className={styles.button} onClick={() => signIn()}>
+          Sign in with Github
+        </button>
       )}
+
       {session && (
-        <div>
-          {console.log(session)}
-          <button onClick={() => signOut()}>
+        <>
+          {console.log(JSON.stringify(session))};
+          <button className={styles.button} onClick={() => signOut()}>
             Sign out ({session.user.name})
           </button>
-          <h1>{session.user.username}</h1>
-          <br></br>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={session.user.image} />
-            <Card.Body>
-              <Card.Title>{session.user.email}</Card.Title>
-            </Card.Body>
-          </Card>
-        </div>
+          <img className={styles.img} variant="top" src={session.user.image} />
+          <h1 className={styles.title}>{session.user.email}</h1>
+          <button className={styles.button} onClick={repoDataURL}>
+            See all Repos
+          </button>
+          <h3 className={styles.title}>{repoData}</h3>
+          {!ifAddRepo && (
+            <button
+              className={styles.button}
+              onClick={() => setIfAddRepo(!ifAddRepo)}
+            >
+              Add new Repo
+            </button>
+          )}
+          {ifAddRepo && (
+            <>
+              {repoNameInput}
+              {repoDescriptionInput}
+              <button className={styles.button} onClick={() => addRepo()}>
+                Save Changes
+              </button>
+              <button
+                className={styles.button}
+                onClick={() => setIfAddRepo(!ifAddRepo)}
+              >
+                Cancel
+              </button>
+            </>
+          )}
+        </>
       )}
     </div>
   );
